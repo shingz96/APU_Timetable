bbeef822ebba5ebb705f627ef7633d17446b948d
shingz96 <cheekahshing96@gmail.com>
2017-08-17 12:16:42 +0800

    Refactor code & only read class details from SQLite Database

--- app/src/main/java/com/shing/aputimetable/model/ApuClassLoader.java
+++ app/src/main/java/com/shing/aputimetable/model/ApuClassLoader.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.content.AsyncTaskLoader;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.shing.aputimetable.entity.ApuClass;
-import com.shing.aputimetable.model.ApuClassContract.ApuClassEntry;
-import com.shing.aputimetable.utils.QueryUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,36 +34,46 @@ public class ApuClassLoader extends AsyncTaskLoader<List<ApuClass>> {
 
     @Override
     public List<ApuClass> loadInBackground() {
-
+        Log.d("Loader Class", "Load in background");
         if (TextUtils.isEmpty(mIntakeCode)) {
             return null;
         }
 
+        List l = getFromSQLite();
+        if (l != null) {
+            return l;
+        }
+
+        return null;
+
+    }
+
+    public List<ApuClass> getFromSQLite() {
         String[] projection = {
-                ApuClassEntry._ID,
-                ApuClassEntry.COLUMN_CLASS_DAY,
-                ApuClassEntry.COLUMN_CLASS_DATE,
-                ApuClassEntry.COLUMN_CLASS_TIME,
-                ApuClassEntry.COLUMN_CLASS_ROOM,
-                ApuClassEntry.COLUMN_CLASS_LOCATION,
-                ApuClassEntry.COLUMN_CLASS_SUBJECT,
-                ApuClassEntry.COLUMN_CLASS_LECTURER
+                ApuClassContract.ApuClassEntry._ID,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_DAY,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_DATE,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_TIME,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_ROOM,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_LOCATION,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_SUBJECT,
+                ApuClassContract.ApuClassEntry.COLUMN_CLASS_LECTURER
         };
 
-        Cursor cursor = mContext.getContentResolver().query(ApuClassEntry.CONTENT_URI, projection, null, null, null);
+        Cursor cursor = mContext.getContentResolver().query(ApuClassContract.ApuClassEntry.CONTENT_URI, projection, null, null, null);
 
         if (cursor != null && cursor.moveToFirst()) {
             List<ApuClass> classes = new ArrayList<>();
             try {
                 while (!cursor.isAfterLast()) {
                     ApuClass c = new ApuClass();
-                    c.setDate(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_DATE)));
-                    c.setDay(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_DAY)));
-                    c.setTime(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_TIME)));
-                    c.setRoom(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_ROOM)));
-                    c.setLocation(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_LOCATION)));
-                    c.setSubject(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_SUBJECT)));
-                    c.setLecturer(cursor.getString(cursor.getColumnIndex(ApuClassEntry.COLUMN_CLASS_LECTURER)));
+                    c.setDate(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_DATE)));
+                    c.setDay(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_DAY)));
+                    c.setTime(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_TIME)));
+                    c.setRoom(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_ROOM)));
+                    c.setLocation(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_LOCATION)));
+                    c.setSubject(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_SUBJECT)));
+                    c.setLecturer(cursor.getString(cursor.getColumnIndex(ApuClassContract.ApuClassEntry.COLUMN_CLASS_LECTURER)));
                     classes.add(c);
                     cursor.moveToNext();
                 }
@@ -73,14 +82,6 @@ public class ApuClassLoader extends AsyncTaskLoader<List<ApuClass>> {
                 cursor.close();
             }
         }
-
-        if (QueryUtils.isNetworkConnected(mContext)) {
-            // Perform the network request, parse the response, and extract a list of apu class details.
-            return QueryUtils.getAllClass(mIntakeCode, mContext);
-        }
-
-
         return null;
-
     }
 }
